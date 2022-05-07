4576481f7aaa40666d4e98996fa8905cb5e0703c
Darshan Hindocha <73314268+darshan-hindocha@users.noreply.github.com>
2022-05-07 23:47:54 +0100

    Create missing_number_in_arithmetic_progression.go

---
+++ golang/missing_number_in_arithmetic_progression.go
@@ -0,0 +1,33 @@
+package main
+
+func missingNumber(arr []int) int {
+    
+    var gap int = arr[1] - arr[0]
+    if gap == 0 {
+        return arr[0]
+    }
+    
+    if gap > 0 {
+        for i:=1; i<len(arr); i++ {
+            n := arr[i]
+            if gap > n - arr[i-1] {
+                return arr[0] + n - arr[i-1]
+            }
+            if gap < n - arr[i-1] {
+                return arr[i-1] + gap
+            }
+        }
+    } else {
+        for i:=1; i<=len(arr); i++ {
+            n := arr[i]
+            if gap < n - arr[i-1] {
+                return arr[0] + n - arr[i-1]
+            }
+            if gap > n - arr[i-1] {
+                return arr[i-1] + gap
+            }
+        }
+    }
+
+    return 0
+}
